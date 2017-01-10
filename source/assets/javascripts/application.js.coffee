50c2849d470226ba4f04e449fd156c814f39f1f3
Thomas Laquay <thomaslaquay@gmail.com>
2017-01-10 17:51:28 +0100

    feat(dropzone) : init dropzone

--- source/assets/javascripts/application.js.coffee
+++ source/assets/javascripts/application.js.coffee
@@ -1,3 +1,4 @@
 #= require "jquery/dist/jquery.min"
-
 #= require "color-extractor"
+#= require dropzone/dist/min/dropzone.min.js
+#= require dropzone.js
---
+++ source/assets/javascripts/dropzone.js
@@ -0,0 +1,16 @@
+$(function () {
+  if(document.contains(document.getElementById("#js-dropzone"))) {
+    //je récupère l'action où sera traité l'upload en PHP
+    var _actionToDropZone = $("#js-dropzone").attr('action');
+
+    //je définis ma zone de drop grâce à l'ID de ma div citée plus haut.
+    Dropzone.autoDiscover = false;
+    var myDropzone = new Dropzone("#js-dropzone", { url: _actionToDropZone });
+
+    myDropzone.on("addedfile", function(file) {
+      console.log('file added', myDropzone);
+    });
+  }
+});
+
+
