45187551253f6d78a04040b38fb7be323a752fab
David Leuliette <dleuliette@gmail.com>
2017-01-19 14:23:43 +0100

    Add download link & 404 page

--- source/assets/javascripts/dropzone.js
+++ source/assets/javascripts/dropzone.js
@@ -2,7 +2,6 @@ $(function () {
   if(document.contains(document.getElementById("#js-dropzone"))) {
     var _actionToDropZone = $("#js-dropzone").attr('action');
 
-    //je définis ma zone de drop grâce à l'ID de ma div citée plus haut.
     Dropzone.autoDiscover = false;
     var myDropzone = new Dropzone("#js-dropzone", { url: _actionToDropZone });
 
